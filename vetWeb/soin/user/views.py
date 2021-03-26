f47e3606544e25af5e4f8b1bbb71b0d403f39b16
BabGee <jeremiahgeorge68@gmail.com>
2021-03-26 18:21:38 +0300

    different user login and signup

--- vetWeb/soin/user/views.py
+++ vetWeb/soin/user/views.py
@@ -11,74 +11,166 @@ from django.views.generic import CreateView
 from django.contrib.auth import get_user_model, logout
 
 from django.contrib.auth import login, authenticate
-from .models import User
+from .models import User, Vet_Officer, Farmer, Student
 from django.contrib.auth.forms import AuthenticationForm   
 
 
 
 User = get_user_model()
 
-class VetOfficerSignUpView(CreateView):
-	def get_success_url(form):
-		return reverse('login')
-    
-	model = User
-	form_class = forms.VetOfficerSignUpForm
-	template_name = 'user/register.html'
+def vet0fficer_signup_view(request):
+	if request.method == 'POST':
+		form = forms.VetOfficerSignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save(commit=False)
+			user.is_vet_officer = True
+			user.first_name = form.cleaned_data.get('first_name')
+			user.last_name = form.cleaned_data.get('last_name')
+			user.email = form.cleaned_data.get('email')
+			user.phone_number = form.cleaned_data.get('phone_number')
+			user.save()
+			vet_officer = Vet_Officer.objects.create(user=user)
+			vet_officer.kvb_number = form.cleaned_data.get('kvb_number')
+			vet_officer.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}. You can now login')
+			return redirect('vet-login')
 
-	def get_context_data(form, **kwargs):
-		context = super(VetOfficerSignUpView, form).get_context_data(**kwargs)
-		context['form'] = form.form_class
-		return context
-    
+	else:
+		form = forms.VetOfficerSignUpForm()
 
-class FarmerSignUpView(CreateView):
-	def get_success_url(form):
-		return reverse('login')
-	model = User
-	form_class = forms.FarmerSignUpForm
-	template_name = 'user/register.html'
+	context = {
+		'form':form
+	}
 
-	def get_context_data(form, **kwargs):
-		context = super(FarmerSignUpView, form).get_context_data(**kwargs)
-		context['form'] = form.form_class
-		return context
+	return render(request, 'user/vetregister.html', context)			
 
 
-class StudentSignUpView(CreateView):
-	def get_success_url(form):
-		return reverse('login')
-        
-	model = User
-	template_name = 'user/register.html'
-	form_class = forms.StudentSignUpForm
-	
-	def get_context_data(form, **kwargs):
-		context = super(StudentSignUpView, form).get_context_data(**kwargs)
-		context['form'] = form.form_class
-		return context
+def farmer_signup_view(request):
+	if request.method == 'POST':
+		form = forms.FarmerSignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save(commit=False)
+			user.is_farmer = True
+			user.first_name = form.cleaned_data.get('first_name')
+			user.last_name = form.cleaned_data.get('last_name')
+			user.email = form.cleaned_data.get('email')
+			user.phone_number = form.cleaned_data.get('phone_number')
+			user.save()
+			farmer = Farmer.objects.create(user=user)
+			farmer.farm_name = form.cleaned_data.get('farm_name')
+			farmer.location = form.cleaned_data.get('location')
+			farmer.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}. You can now login')
+			return redirect('farmer-login')
+
+	else:
+		form = forms.FarmerSignUpForm()
+
+	context = {
+		'form':form
+	}
+
+	return render(request, 'user/farmerregister.html', context)
+
+
+def student_signup_view(request):
+	if request.method == 'POST':
+		form = forms.StudentSignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save(commit=False)
+			user.is_student = True
+			user.first_name = form.cleaned_data.get('first_name')
+			user.last_name = form.cleaned_data.get('last_name')
+			user.email = form.cleaned_data.get('email')
+			user.phone_number = form.cleaned_data.get('phone_number')
+			user.save()
+			student = Student.objects.create(user=user)
+			student.farm_name = form.cleaned_data.get('farm_name')
+			student.location = form.cleaned_data.get('location')
+			student.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}. You can now login')
+			return redirect('student-login')
+
+	else:
+		form = forms.StudentSignUpForm()
+
+	context = {
+		'form':form
+	}
+
+	return render(request, 'user/studentregister.html', context)
 		
 
-def user_login(request):
-    form = AuthenticationForm()
-    if request.method == 'POST':  
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            if user.is_authenticated and user.is_vet_officer:
-               return redirect('vet-portal')
-            elif user.is_authenticated and user.is_farmer:
-                return redirect('farmer-portal')
-            elif user.is_authenticated and user.is_student:
-                return redirect('student-portal')    
-        else:
-            messages.error(request, 'invalid Credentials')
+def vet_login(request):
+	form = AuthenticationForm()
+	if request.method == 'POST':  
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			if user.is_authenticated and user.is_vet_officer:
+				return redirect('vet-portal')
+			elif user.is_authenticated and user.is_farmer:
+				messages.warning(request, 'Kindly login as farmer')
+				return redirect('farmer-login')
+			elif user.is_authenticated and user.is_student:
+				messages.warning(request, 'Kindly login as student')
+				return redirect('student-login')
+				    
+		else:
+			messages.error(request, 'invalid Credentials')
     
-    return render(request, 'user/login.html', {'form':form})
+	return render(request, 'user/vetlogin.html', {'form':form})
+
+def farmer_login(request):
+	form = AuthenticationForm()
+	if request.method == 'POST':  
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			if user.is_authenticated and user.is_farmer:
+				return redirect('farmer-portal')
+			elif user.is_authenticated and user.is_vet_officer:
+				messages.warning(request, 'Kindly login as Vet Officer')
+				return redirect('vet-login')
+			elif user.is_authenticated and user.is_student:
+				messages.warning(request, 'Kindly login as student')
+				return redirect('student-login')
+				    
+		else:
+ 			messages.error(request, 'invalid Credentials')
+    
+	return render(request, 'user/farmerlogin.html', {'form':form})
+
+def student_login(request):
+	form = AuthenticationForm()
+	if request.method == 'POST':  
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			if user.is_authenticated and user.is_student:
+				return redirect('student-portal')
+			elif user.is_authenticated and user.is_vet_officer:
+				messages.warning(request, 'Kindly login as Vet Officer')
+				return redirect('vet-login')
+			elif user.is_authenticated and user.is_farmer:
+				messages.warning(request, 'Kindly login as Farmer')
+				return redirect('farmer-login')
+				    
+		else:
+			messages.error(request, 'invalid Credentials')
+    
+	return render(request, 'user/studentlogin.html', {'form':form})
 
 def user_logout(request):
     logout(request)
     messages.success(request, 'Successfully logged out')
-    return redirect('login')
+    return redirect('index')
